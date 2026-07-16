138d06bedfa55d8604c542c640204be749b0f3a3
riobatubara <outrageryo@gmail.com>
2026-07-17 06:12:52 +0700

    chore: lock contention is gone

--- utils/logger.go
+++ utils/logger.go
@@ -1,11 +1,37 @@
 package utils
 
 import (
-	"log"
+	"os"
+	"strconv"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-// LogEvent prints a formatted terminal log for each emitted event
+var logger *zap.Logger
+
+func init() {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = ""
+	encoderConfig.LevelKey = ""
+	encoderConfig.CallerKey = ""
+	encoderConfig.MessageKey = "msg"
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		zapcore.Lock(zapcore.AddSync(zapcore.Lock(os.Stderr))), // Thread-safe standard error output
+		zap.InfoLevel,
+	)
+
+	logger = zap.New(core)
+}
+
+// LogEvent preserves your exact signature and output format
 func LogEvent(index int, tsclient int64, sessionID, event, value string) {
-	log.Printf("data[%d]:: tsclient: %d, sessid: %s, event: %s, value: %s",
-		index, tsclient, sessionID, event, value)
+	// Constructing the exact string pattern using fast string interpolation
+	// data[%s]:: tsclient: %s, sessid: %s, event: %s, value: %s
+	msg := "data[" + strconv.Itoa(index) + "]:: tsclient: " + strconv.FormatInt(tsclient, 10) +
+		", sessid: " + sessionID + ", event: " + event + ", value: " + value
+
+	logger.Info(msg)
 }
